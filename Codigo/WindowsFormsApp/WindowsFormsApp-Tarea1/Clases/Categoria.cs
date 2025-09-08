18399bf7f6824a13fc563585d17bea7f93ff7be1
RaulLuppini <raulluppini@gmail.com>
2025-09-08 19:49:30 -0300

    Clase categoria e Imagen

---
+++ Codigo/WindowsFormsApp/WindowsFormsApp-Tarea1/Clases/Categoria.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp_Tarea1
+{
+    internal class Categoria
+    {
+        private int CodigoCategoria;
+        private string NombreCategoria;
+
+        public int codigoCategoria { get => CodigoCategoria; set => CodigoCategoria= value; }
+        public string nombreCategoria { get => NombreCategoria; set =>  NombreCategoria= value; }
+
+    }
+}
---
+++ Codigo/WindowsFormsApp/WindowsFormsApp-Tarea1/Clases/Imagen.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp_Tarea1
+{
+    internal class Imagen
+    {
+        private int CodigoImagen;
+        private string Url;
+        private int ArticuloCodigo;
+
+        public int codigoImagen { get => CodigoImagen; set => CodigoImagen = value; }
+        public string url { get => Url; set => Url = value; }
+        public int articuloCodigo { get => ArticuloCodigo; set => ArticuloCodigo = value; }
+
+    }
+}
